206d39244208351e7fcfbb60ded318ac335b3ec1
Aman1991Codez <kitefly1991@gmail.com>
2022-11-15 17:43:28 +0530

    first and completed commit

---
+++ application/config/routes.php
@@ -0,0 +1,18 @@
+
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
+
+$route['project'] = "project/index";
+$route['project/create'] = "project/create";
+$route['project/store']['post'] = "project/store";
+$route['project/edit/(:num)'] = "project/edit/$1";
+$route['project/update/(:num)']['put'] = "project/update/$1";
+$route['project/delete/(:num)']['delete'] = "project/delete/$1";
+
+$route['upload-image'] = 'imageuploadcontroller';
+$route['store-image'] = 'imageuploadcontroller/store';
